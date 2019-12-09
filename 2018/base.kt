a87b7164abef788458f968e42e4259b450cf8cc2
Serge Knystautas <sergek@lokitech.com>
2019-12-09 00:10:33 -0800

    Base kotlin file that has the starting point for any challenge

---
+++ 2018/base.kt
@@ -0,0 +1,26 @@
+package aoc2018;
+
+import java.io.File;
+import kotlin.system.exitProcess;
+
+fun Loadcodes(filename: String) : List<String> {
+    return File(filename).readLines();
+}
+
+fun Splitcodes(opcodes: String): List<Int> {
+    // println("you gave ops of $opcodes");
+    var ops: List<Int> = opcodes.split(",").map{ it.trim().toInt() };
+    // println("This is ${ops.size} instructions");
+    return ops;
+}
+
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("You done messed up a-a-ron");
+        println("Usage: cmd filename");
+        exitProcess(-1);
+    }
+    var lines = Loadcodes(args[0]);
+
+    println(lines);
+}
